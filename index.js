b24b5243b2fe2c06eecd671445af88974ccf7c38
Pankaj <rcypankaj@gmail.com>
2023-03-28 01:13:11 +0530

    added header and home section

--- index.js
+++ index.js
@@ -6,9 +6,48 @@ let searchBar = document.querySelector(".search-bar-container");
 window.onscroll = () => {
   searchBtn.classList.remove("fa-times");
   searchBar.classList.remove("active");
+  menu.classList.remove("fa-times"); // p
+  navbar.classList.remove("active"); // p
+  loginForm.classList.remove("active"); // p
 };
+
 searchBtn.addEventListener("click", () => {
   searchBtn.classList.toggle("fa-times");
   searchBar.classList.toggle("active");
 });
 // Headers Section javascript End
+
+// Login section start
+let formBtn = document.querySelector("#login-btn");
+let loginForm = document.querySelector(".login-form-container");
+let formClose = document.querySelector("#form-close");
+
+formBtn.addEventListener("click", () => {
+  loginForm.classList.add("active");
+});
+
+formClose.addEventListener("click", () => {
+  loginForm.classList.remove("active");
+});
+// Login section End
+
+// Home Section start
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+});
+
+let videoBtn = document.querySelectorAll(".vid-btn");
+
+videoBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".controls .active").classList.remove("active");
+    btn.classList.add("active");
+    let src = btn.getAttribute("data-src");
+    document.querySelector("#video-slider").src = src;
+  });
+});
+// Home Section end
